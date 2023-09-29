509eead1bb0099f0f123ffedfa962e32713e8314
Mohamed Al-Marzuk <mohamed.al-marzuk@usach.cl>
2023-09-29 09:45:36 -0300

    Se Agrega el RF6 chatbot-add-flow

--- TDAChatbot_22594262_Al-Marzuk.rkt
+++ TDAChatbot_22594262_Al-Marzuk.rkt
@@ -34,3 +34,29 @@ Descripción: Función que comprueba si en una lista de flows de un chatbot se r
 
 (define (id-repetido? id flows)
   (not (null? (filter (lambda (flow) (= id (car flow))) flows))))
+
+#|
+RF6: TDA Chatbot (Modificador)
+Dominio: Chatbot, Flow
+Recorrido: Chatbot
+Tipo de recursion: de cola
+Descripción: Función que agrega recursivamente un flow a un chatbot haciendo recursion de cola.
+Se comprueba si es nula la lista de flows, si es así, se devuelve un chatbot que contenga como lista
+de flows la variable auxiliar "list-flow", y si no es nula, se comprueba si el id del flow no se repite en base
+a su id, si no se repite, se agrega el car de flow a list-flow y se hace el llamado recursivo  con el mismo chatbot,
+pero con el cdr de la lista de flows. Si se repite el id, se devuelve el chatbot original sin ningun cambio.
+LA función exterior se llama con el chatbot y una lista que tenga como unico elemento el flow
+|#
+
+(define (chatbot-add-flow chatbot flow)  
+  (define (add-flow2 chatbot list-flow)    
+    (if (null? (last chatbot))        
+        (list (car chatbot) (cadr chatbot) (caddr chatbot) (cadddr chatbot) list-flow)        
+        (if (not (id-repetido? (car flow) (last chatbot)))
+                          (add-flow2 (list (car chatbot) (cadr chatbot) (caddr chatbot)
+                                          (cadddr chatbot) (cdr (last chatbot)))
+                   (append (list (car (last chatbot))) list-flow))                          
+                          chatbot)))
+  (add-flow2 chatbot (list flow)))
+
+
